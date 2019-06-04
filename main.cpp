9d9758599e998c2726f3fa11776a033c3bb7da85
jolanxiao <jolanxiao@gmail.com>
2019-06-04 19:24:22 +0800

    add main

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: main.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 二  6/ 4 19:15:35 2019
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main(int argc, char *argv[])
+{
+    return 0;
+}
